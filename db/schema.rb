354e1124434e0c77becf7bab3293338a3483bc8c
Shahina Rahman <shahinarahman101@gmail.com>
2015-08-05 00:24:21 +0100

    changed colour to price in products controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150804222648) do
+ActiveRecord::Schema.define(version: 20150804225713) do
 
   create_table "products", force: true do |t|
     t.string   "name"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20150804222648) do
     t.string   "image_url"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "Difficulty"
+    t.string   "price"
   end
 
 end
